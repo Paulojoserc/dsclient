329551065b9fa642a946936793233077508a0237
Paulojoserc <pjrc83@gmail.com>
2022-10-19 22:16:36 -0300

    Classe entity e DTO  feita

--- backend/dsclient/src/main/java/com/pjcosta/dsclient/resources/exceptions/ResourceExceptionHandler.java
+++ backend/dsclient/src/main/java/com/pjcosta/dsclient/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.pjcosta.dsclient.services.exception.DatabaseException;
 import com.pjcosta.dsclient.services.exception.ResourceNotFoundException;
 
 @ControllerAdvice
